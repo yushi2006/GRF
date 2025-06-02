5238545b2f1aeddf288e83aa50cc97e988cb0b7d
Yusuf Shihata <yusufshihata2006@gmail.com>
2025-06-02 14:47:24 +0300

    starting my reseearch journey

---
+++ src/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+
+# Self-Attention block
+class MultiSelfAttention(nn.Module):
+    def __init__(self, embedding_size):
+        super(MultiSelfAttention, self).__init__()
+
+# Cross-Attention Block
+class MultiCrossAttention(nn.Module):
+    def __init__(self):
+        super(MultiCrossAttention, self).__init__()
+
+# FeedForward Block
+class FeedForward(nn.Module):
+    def __init__(self):
+        super(FeedForward, self).__init__()
+
+class ModalityAwareFusion(nn.Module):
+    def __init__(self):
+        super(ModalityAwareFusion, self).__init__()
